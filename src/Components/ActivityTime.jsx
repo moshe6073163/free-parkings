aca0ee620ac9849971503bc35f289176856b6558
moshe <moshe6073163@gmail.com>
2023-04-02 09:31:28 +0300

    m

--- src/Components/ActivityTime.jsx
+++ src/Components/ActivityTime.jsx
@@ -21,7 +21,7 @@ export default function ActivityTime(props) {
 
     function setActive(){
         const days = [
-           {start: startSun.current.value, end: endSun.current.value},
+            {start: startSun.current.value, end: endSun.current.value},
             {start: startMon.current.value, end: endMon.current.value},
             {start: startTues.current.value, end: endTues.current.value},
             {start: startWed.current.value, end: endWed.current.value},
@@ -36,13 +36,13 @@ export default function ActivityTime(props) {
 
   return (
     <div className='row d-flex flex-column align-items-center'>
-        <div className='m-2' >Sunday: <input type="time" ref={startSun} /><input type="time" ref={endSun} /></div>
-        <div className='m-2' >Monday: <input type="time" ref={startMon} /><input type="time" ref={endMon} /></div>
-        <div className='m-2' >Tuesday: <input type="time" ref={startTues} /><input type="time" ref={endTues} /></div>
-        <div className='m-2' >Wednesday: <input type="time" ref={startWed} /><input type="time" ref={endWed} /></div>
-        <div className='m-2' >Thursday: <input type="time" ref={startThur} /><input type="time" ref={endThur} /></div>
-        <div className='m-2' >Friday: <input type="time" ref={startFri} /><input type="time" ref={endFri} /></div>
-        <div className='m-2' >Saturday: <input type="time" ref={startSat} /><input type="time" ref={endSat} /></div>
+        <div className='m-2 w-75 d-flex justify-content-end' >Sunday: <input className='col-3 mx-1' type="time" ref={startSun} /><input className='col-3 mx-1' type="time" ref={endSun} /></div>
+        <div className='m-2 w-75 d-flex justify-content-end' >Monday: <input className='col-3 mx-1' type="time" ref={startMon} /><input className='col-3 mx-1' type="time" ref={endMon} /></div>
+        <div className='m-2 w-75 d-flex justify-content-end' >Tuesday: <input className='col-3 mx-1' type="time" ref={startTues} /><input className='col-3 mx-1' type="time" ref={endTues} /></div>
+        <div className='m-2 w-75 d-flex justify-content-end' >Wednesday: <input className='col-3 mx-1' type="time" ref={startWed} /><input className='col-3 mx-1' type="time" ref={endWed} /></div>
+        <div className='m-2 w-75 d-flex justify-content-end' >Thursday: <input className='col-3 mx-1' type="time" ref={startThur} /><input className='col-3 mx-1' type="time" ref={endThur} /></div>
+        <div className='m-2 w-75 d-flex justify-content-end' >Friday: <input className='col-3 mx-1' type="time" ref={startFri} /><input className='col-3 mx-1' type="time" ref={endFri} /></div>
+        <div className='m-2 w-75 d-flex justify-content-end' >Saturday: <input className='col-3 mx-1' type="time" ref={startSat} /><input className='col-3 mx-1' type="time" ref={endSat} /></div>
         <div className='btn btn-light w-25 mt-2 mb-2' onClick={setActive}>Set</div>
     </div>
   )
